3a2a090852beae2ae45023c30723cce3938f4d4f
Ram-ziggandzagg <ram.wadher@ziggandzagg.com>
2026-02-03 00:05:52 +0000

    Update script.js
    Add first version of code

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const enterBtn = document.getElementById("enterBtn");
+const intro = document.getElementById("intro");
+const site = document.getElementById("site");
+
+enterBtn.addEventListener("click", () => {
+  intro.classList.add("open");
+
+  setTimeout(() => {
+    intro.style.display = "none";
+    site.style.display = "block";
+    document.body.style.overflow = "auto";
+  }, 1400);
+});
 
